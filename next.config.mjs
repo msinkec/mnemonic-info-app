f2ba4acbe3177bb956c58ee6ea87e6dea20e142a
msinkec <sinkec.mihael@gmail.com>
2024-06-03 09:02:09 +0200

    update gh pages deploy method

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
-
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  assetPrefix: isProd ? '/mnemonic-info-app/' : '',
+  output: 'export',
+  basePath: '/mnemonic-info-app',
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.resolve.fallback = {
